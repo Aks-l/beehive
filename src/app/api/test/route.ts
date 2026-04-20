7beed7ea99692b8c3231c609c8d27713492888e4
0herm <0herm@protonmail.com>
2026-04-20 21:05:36 +0200

    remove debug

--- src/app/api/test/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server'
-
-export async function GET(request: NextRequest) {
-    const protocol = request.headers.get('x-forwarded-proto') ?? 'error'
-    return NextResponse.json({ protocol })
-}
